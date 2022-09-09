c1ae0e956b35a25b7386b625fd16f916cacad367
Immanuel-john <imjjohn18@gmail.com>
2022-09-09 12:19:33 +0530

    solana program done

--- crowd-funding/program/src/lib.rs
+++ crowd-funding/program/src/lib.rs
@@ -12,6 +12,7 @@ use solana_program::{
     rent::Rent,
     sysvar::Sysvar,
 };
+use borsh::{BorshDeserialize, BorshSerialize};
 
 
 // Every solana program has one entry point
@@ -68,6 +69,48 @@ fn create_campaign(
     accounts: &[AccountInfo],
     instruction_data: &[u8]
 ) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    
+    /// Writing account or we can call it program account.
+    /// This is an account we will create in our front-end.
+    /// This account should br owned by the solana program.
+    let writing_account = next_account_info(accounts_iter)?;
+
+    /// Account of the person creating the campaign.
+    let creator_account = next_account_info(accounts_iter)?;
+
+    // Now to allow transactions we want the creator account to sign the transaction.
+    if !creator_account.is_signer {
+        msg!("creator_account should be signer");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    /// We want to write in this account so we want its owner by the program.
+    if writing_account.owner != program_id {
+        msg!("writing_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let mut input_data = CampaignDetails::try_from_slice(&instruction_data)
+    .expect("Instruction data serialization didn't worked");
+
+
+    if input_data.admin != *creator_account.key {
+        msg!("Invaild instruction data");
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    // get the minimum balance we need in our program account.
+    let rent_exemption = Rent::get()?.minimum_balance(writing_account.data_len());
+    /// And we make sure our program account (`writing_account`) has that much lamports(balance).
+    if **writing_account.lamports.borrow() < rent_exemption {
+        msg!("The balance of writing_account should be more then rent_exemption");
+        return Err(ProgramError::InsufficientFunds);
+    }
+    // Then we can set the initial amount donate to be zero.
+    input_data.amount_donated=0;
+
+    input_data.serialize(&mut &mut writing_account.data.borrow_mut()[..])?;
+
     Ok(())
 }
 
@@ -76,6 +119,42 @@ fn withdraw(
     accounts: &[AccountInfo],
     instruction_data: &[u8]
 ) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let writing_account = next_account_info(accounts_iter)?;
+    let admin_account = next_account_info(accounts_iter)?;
+    
+    // We check if the writing account is owned by program.
+    if writing_account.owner != program_id {
+        msg!("writing_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    // Admin account should be the signer in this trasaction.
+    if !admin_account.is_signer {
+        msg!("admin should be signer");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    let campaign_data = CampaignDetails::try_from_slice(*writing_account.data.borrow())
+    .expect("Error deserializing data");
+
+    if campaign_data.admin != *admin_account.key {
+        msg!("Only the account admin can withdraw");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let input_data = WithdrawRequest::try_from_slice(&instruction_data)
+        .expect("Instruction data serialization didn't worked");
+
+    let rent_exemption = Rent::get()?.minimum_balance(writing_account.data_len());
+
+    if **writing_account.lamports.borrow() - rent_exemption < input_data.amount {
+        msg!("Insufficent balance");
+        return Err(ProgramError::InsufficientFunds);
+    }
+
+
+    **writing_account.try_borrow_mut_lamports()? -= input_data.amount;
+    **admin_account.try_borrow_mut_lamports()? += input_data.amount;
+
     Ok(())
 }
 
@@ -84,8 +163,49 @@ fn donate(
     accounts: &[AccountInfo],
     instruction_data: &[u8]
 ) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let writing_account = next_account_info(accounts_iter)?;
+    let donator_program_account = next_account_info(accounts_iter)?;
+    let donator = next_account_info(accounts_iter)?;
+
+    if writing_account.owner != program_id {
+        msg!("writing_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    if donator_program_account.owner != program_id {
+        msg!("donator_program_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    if !donator.is_signer {
+        msg!("donator should be signer");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    let mut campaign_data = CampaignDetails::try_from_slice(*writing_account.data.borrow())
+        .expect("Error deserializing data");
+
+    campaign_data.amount_donated += **donator_program_account.lamports.borrow();
+    **writing_account.try_borrow_mut_lamports()? += **donator_program_account.lamports.borrow();
+    **donator_program_account.try_borrow_mut_lamports()? = 0;
+
+    campaign_data.serialize(&mut &mut writing_account.data.borrow_mut()[..])?;
+
     Ok(())
 }
 
 // Then we call the entry point macro to add `process_instruction` as our entry point to our program.
 entrypoint!(process_instruction);
+
+
+#[derive( BorshSerialize, BorshDeserialize, Debug)]
+struct CampaignDetails {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub image_lin: String,
+    pub amount_donated: u64,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct WithdrawRequest {
+    pub amount: u64,
+}
